c566ab6cae180c54b8e8e17cf61f29a34414e265
ArgsKwargs <Marvin.mangold00@googlemail.com>
2021-01-07 07:28:56 +0100

    rewritten readudt.py

--- readudt.py
+++ readudt.py
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-import re, time
+import re
 
 
 # define possible Datatypes and its bit-size
